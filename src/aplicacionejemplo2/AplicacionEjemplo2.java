be98a0b50c06c674661deb79d0484e91d5634d8d
jbarrosgarcia <jbarrosgarcia@atanasoff07.danielcastelao.org>
2015-10-15 10:32:52 +0200

    elimine los comentarios

--- src/aplicacionejemplo2/AplicacionEjemplo2.java
+++ src/aplicacionejemplo2/AplicacionEjemplo2.java
@@ -8,16 +8,17 @@ public class AplicacionEjemplo2 {
 
     
     public static void main(String[] args) {
-//      Scanner teclado=new Scanner(System.in); 
-//      float base,altura;
-//      System.out.println("Introduzca base");
-//      base=teclado.nextFloat();
-//      System.out.println("Introduzca altura");
-//      altura=teclado.nextFloat();
-      Rectangulo rec1=new Rectangulo(3,5);
+      
+      Scanner teclado=new Scanner(System.in); 
+      float base,altura;
+      System.out.println("Introduzca base");
+      base=teclado.nextFloat();
+      System.out.println("Introduzca altura");
+      altura=teclado.nextFloat();
+      Rectangulo rec1=new Rectangulo(base,altura);
       System.out.println("Base = "+rec1.getBase());
       System.out.println("Alutra = "+rec1.getAltura());
-      System.out.println("Area = "+rec1.calcularArea(7, 8)+" metros cuadrados");
+      System.out.println("Area = "+rec1.calcularArea(base, altura)+" metros cuadrados");
       System.out.println("Perimetro = "+rec1.calcularPerimetro()+" metros");
       
     }
